acb26730260df8b026e052cf3dcda0da207da27b
ihatetn931 <shadowpot@gmail.com>
2025-01-08 14:32:24 -0600

    First Push

---
+++ BepinEx.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace BetterDeepMiner
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "modder.ihatetn931.StationeersMods.BetterDeepMiner";
+        public const string PLUGIN_NAME = "BetterDeepMiner";
+        public const string PLUGIN_VERSION = "1.0.2";
+
+        internal static ConfigEntry<bool> toggleBetterDeepMiner;
+        internal static ConfigEntry<bool> toggleBetterDeepMinerIce;
+        internal static ConfigEntry<bool> GetOres;
+
+        void Awake()
+        {
+            Logger.LogInfo($"Plugin {PLUGIN_NAME} is loading!");
+            Harmony harmony = new Harmony(PLUGIN_GUID);
+            harmony.PatchAll();
+            AddSettings();
+            Logger.LogInfo($"Plugin {PLUGIN_NAME} Verison {PLUGIN_VERSION} is loaded!");
+        }
+
+        void AddSettings()
+        {
+            toggleBetterDeepMiner = Config.Bind("BetterDeepMiner General", "Enable BetterDeepMiner", true, new ConfigDescription("Enable BetterDeepMiner To Use all the settings on this config", null, new ConfigurationManagerAttributes { Browsable = true }));
+            toggleBetterDeepMinerIce = Config.Bind("BetterDeepMiner General", "Enable BetterDeepMiner Ice", true, new ConfigDescription("Enable BetterDeepMiner to output ice and dirty ore", null, new ConfigurationManagerAttributes { Browsable = true }));
+            GetOres = Config.Bind("BetterDeepMiner General", "Enable BetterDeepMiner Ores", true, new ConfigDescription("With this enabled you will get ores and not dirty ore but takes longer", null, new ConfigurationManagerAttributes { Browsable = true }));
+        }
+    }
+}
+
---
+++ BetterDeepMiner.cs
@@ -0,0 +1,71 @@
+﻿using Assets.Scripts.Objects.Items;
+using Assets.Scripts.Objects.Pipes;
+using Assets.Scripts;
+using HarmonyLib;
+using Assets.Scripts.Objects;
+
+namespace BetterDeepMiner
+{
+    internal class DeepMinerPatches
+    {
+        [HarmonyPatch(typeof(DeepMiner), nameof(DeepMiner.SpawnOre))]
+        public static class DeepMiner_SpawnOre_Patch
+        {
+            [HarmonyPrefix]
+            static bool Prefix(DeepMiner __instance)
+            {
+                __instance._oreSpawnTimeMax = 1;
+                __instance._oreSpawnTimeMin = 0.1f;
+                __instance._oreSpawnTime = 1;
+                if (Plugin.toggleBetterDeepMiner.Value)
+                {
+                    System.Random rndIce = new System.Random();
+                    int randIce = rndIce.Next(Ice.AllIcePrefabs.Count);
+                    if (number(0, 3) == 0)
+                    {
+                        DirtyOre dirtyOre = Thing.Create<DirtyOre>(DeepMiner._spawnOrePrefab, __instance.ExportSlot.Location);
+                        dirtyOre.SetQuantity(UnityEngine.Random.Range(__instance._oreAmountPerSpawnMin, __instance._oreAmountPerSpawnMax + 1));
+                        dirtyOre.ParentSlot = null;
+                        OnServer.MoveToSlot(dirtyOre, __instance.ExportSlot);
+                    }
+                    if (Plugin.toggleBetterDeepMinerIce.Value)
+                    {
+                        if (number(0, 3) == 1)
+                        {
+                            Ice iceOre = Thing.Create<Ice>(Ice.AllIcePrefabs[randIce], __instance.ExportSlot.Location);
+                            iceOre.SetQuantity(UnityEngine.Random.Range(__instance._oreAmountPerSpawnMin, __instance._oreAmountPerSpawnMax + 1));
+                            iceOre.ParentSlot = null;
+                            OnServer.MoveToSlot(iceOre, __instance.ExportSlot);
+                        }
+                    }
+                    if (Plugin.GetOres.Value)
+                    {
+                        if (number(0, 3) == 1)
+                        {
+                            Ore processedOre = Thing.Create<Ore>(Ore.AllOrePrefabs[randIce], __instance.ExportSlot.Location);
+                            processedOre.SetQuantity(UnityEngine.Random.Range(__instance._oreAmountPerSpawnMin, __instance._oreAmountPerSpawnMax + 1));
+                            processedOre.ParentSlot = null;
+                            OnServer.MoveToSlot(processedOre, __instance.ExportSlot);
+                        }
+                    }
+                    __instance._timeSinceLastOreSpawn = GameManager.GameTime;
+                }
+                else
+                {
+                    DirtyOre dirtyOre = Thing.Create<DirtyOre>(DeepMiner._spawnOrePrefab, __instance.ExportSlot.Location);
+                    dirtyOre.SetQuantity(UnityEngine.Random.Range(__instance._oreAmountPerSpawnMin, __instance._oreAmountPerSpawnMax + 1));
+                    dirtyOre.ParentSlot = null;
+                    OnServer.MoveToSlot(dirtyOre, __instance.ExportSlot);
+                    __instance._timeSinceLastOreSpawn = GameManager.GameTime;
+                }
+                return false;
+            }
+
+            static int number(int min, int max)
+            {
+                System.Random random = new System.Random();
+                return random.Next(min, max);
+            }
+        }
+    }
+}
